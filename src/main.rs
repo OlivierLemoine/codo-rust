a547627f44def021b20200ef8778ff64c4b9014d
olivierlemoine <olivier.lemoine@sii.fr>
2019-07-05 15:50:39 +0200

    Add main

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,13 @@
-fn main() {}
+#[derive(Debug)]
+struct Todo {
+    name: String,
+    is_checked: bool,
+}
+
+fn main() {
+    let mut todos: Vec<Todo> = vec![];
+    todos.push(Todo {
+        name: "a".into(),
+        is_checked: false,
+    });
+}
